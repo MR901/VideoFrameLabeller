0a55882380ba2e3d1ed3027e031c97e70d381a8a
Mohit Rajput <mohit_rajput@cargill.com//mohitrajput901@gmail.com>
2021-03-06 20:50:20 +0530

    working code

--- src/run.py
+++ src/run.py
@@ -1,7 +1,7 @@
 
 ## Configs
 import pyglet
-import os
+import os, time
 # pyglet.lib.load_library('avbin')
 # pyglet.have_avbin=True
 
@@ -14,8 +14,6 @@ config_di = {
     'saving_dir': '../data/output/',
     'save_every_nth_frame': 5,
 }
-
-
 
 ## Internal Variable
 input_path = config_di['input_vid_path']
@@ -37,12 +35,27 @@ if ext not in vid_formats:
     
 ## Initializing 
 # https://stackoverflow.com/questions/4986662/taking-a-screenshot-with-pyglet-fixd
-window= pyglet.window.Window()
+
+source = pyglet.media.load(input_path)
+fmt = source.video_format
+window = pyglet.window.Window(width=fmt.width, height=fmt.height, fullscreen=False, resizable=True, caption='Video')
+window.set_mouse_visible(False)
+player = pyglet.media.Player()
+player.queue(source)
+time.sleep(5)
+player.play()
+
+'''
+width, height, title = 500, 500, 'Player Window'
+window= pyglet.window.Window(width, height, title)
+# window= pyglet.window.Window()
 player = pyglet.media.Player()
 source = pyglet.media.StreamingSource()
 MediaLoad = pyglet.media.load(input_path)
 player.queue(MediaLoad)
 player.play()
+'''
+
 
 
 def generate_frame_name(config, prefix=None, frame_counter=None, category=None):
@@ -59,14 +72,14 @@ def generate_frame_name(config, prefix=None, frame_counter=None, category=None):
     prefix = '' if prefix is None else str(prefix)
     frame_cnt = '' if frame_counter is None else '_fr'+format(frame_counter, '06d') 
     ts_val = '_ts'+ str(round(player.time,3))
-    category = default_category if category is None else '_cls'+str(category)
+    category = default_category if category is None else str(category).lower()
     saving_dir= config['saving_dir']
     vid_name = '.'.join(config['input_vid_path'].split('/')[-1].split('.')[:-1])
     
     if 'screenshot' in prefix.lower():
-        saving_path = f'{saving_dir}{vid_name}/Screenshots/{prefix}{frame_cnt}{ts_val}{category}.png'
+        saving_path = f'{saving_dir}{vid_name}/Screenshots/{prefix}{vid_name}{frame_cnt}{ts_val}_cls{category}.png'
     else:
-        saving_path = f'{saving_dir}{vid_name}/{category}/{prefix}{frame_cnt}{ts_val}{category}.png'
+        saving_path = f'{saving_dir}{vid_name}/{category}/{prefix}{vid_name}{frame_cnt}{ts_val}_cls{category}.png'
     
     ## creeating dir if it doeesn't exist
     if os.path.exists('/'.join(saving_path.split('/')[:-1])) is False:
@@ -76,26 +89,32 @@ def generate_frame_name(config, prefix=None, frame_counter=None, category=None):
 
 
 
+# fps_display = pyglet.clock.ClockDisplay(
+#     format='%(fps).1f',
+#     color=(0.5, 0.5, 0.5, 1)
+#     )
+
 @window.event
 def on_draw():
-    ''' 
-    '''
-    window.clear()
-    
+    '''  '''    
     ## getting variables
     global frame_counter, config_di, category
     frame_counter += 1
     
-    ## adding text on frame
-    label = pyglet.text.Label(f'{frame_counter} {category}',
-                          font_name='Times New Roman',
-                          font_size=36,
-                          x=window.width//2, y=window.height//2,
-                          anchor_x='center', anchor_y='center')
-    label.draw()
-    
+    if frame_counter%25==0:
+        print(f'---| Current category: {category}\t Current Frame No.: {frame_counter}')
+        
     ## skip if not nth multiple
     if frame_counter % config_di['save_every_nth_frame'] != 0: return 
+
+    ## adding text on frame
+    # label = pyglet.text.Label(f'{frame_counter} {category}',
+    #                       font_name='Times New Roman',
+    #                       font_size=36,
+    #                       x=window.width//2, y=window.height//2,
+    #                       anchor_x='center', anchor_y='center')
+    # label.draw()
+    # fps_display.draw()
     
     ## if continued then savee frame
     saving_path = generate_frame_name(
@@ -108,10 +127,10 @@ def on_draw():
     ## saving image
     pyglet.image.get_buffer_manager().get_color_buffer().save(saving_path)
     
+    ## Clear the window 
+    # window.clear() -- causes the video to pixelate
     if player.source and player.source.video_format:
-        player.get_texture().blit(250,250)
-
-
+        player.get_texture().blit(0,0,width=window.width, height=window.height)
 
 @window.event
 def on_key_press(symbol, modifier):
@@ -128,37 +147,116 @@ def on_key_press(symbol, modifier):
     global category, default_category
     
     if symbol==pyglet.window.key.W: 
-        ## pause and label
+        print('Video Paused and label will be added.')
         player.pause()
         category = input('Enter category name: ')
     elif symbol==pyglet.window.key.S: 
-        ## pause and label
+        print('Label under current stopped.')
+        category = default_category
         player.pause()
     elif symbol==pyglet.window.key.D:
-        ## resume
-        player.play() 
+        print('Video Resumed.')
+        player.play()
     elif symbol==pyglet.window.key.A:
-        ## stop
-        category = default_category
+        print('Video Paused.')
+        player.pause()
+    elif symbol==pyglet.window.key.ESCAPE: #[Esc]
+        print('Exiting...')
+        time.sleep(3)
+        window.close()
+        exit()
     
-    elif symbol==pyglet.window.key.Q:
+    elif symbol==pyglet.window.key.M:
         ## TAKE SCREENSHOT
-        saving_name = generate_frame_name(config, prefix='Screenshot', frame_counter=None, category=None)
-        pyglet.image.get_buffer_manager().get_color_buffer().save(saving_path)
+        saving_name = generate_frame_name(config_di, prefix='Screenshot', frame_counter=None, category=None)
+        pyglet.image.get_buffer_manager().get_color_buffer().save(saving_name)
 
 
-
-
-print(    ''' 
+print(''' 
     Every "{}" th frame  will be saved 
     
-    W : Pause and Add Label
-    S : Just Pause the Video
-    D : Resume with the same / Current Label
-    A : Stop the Label and replace with default label
+    W : Pause + Add Label
+    S : Stop Category Label
+    D : Resume Current Label
+    A : Pause
     
-    Q : Take Screenshot of the current Frame
+    M : Take Screenshot of the current Frame
     '''.format(config_di['save_every_nth_frame']))
 # pyglet.clock.schedule(update) # cause a timed event as fast as you can!
 pyglet.app.run()
 
+
+
+'''
+import pyglet
+from pyglet.gl import *
+from threading import *
+
+# REQUIRES: AVBin
+#pyglet.options['audio'] = ('alsa', 'openal', 'silent')
+key = pyglet.window.key
+
+class main(pyglet.window.Window):
+    def __init__ (self):
+        super(main, self).__init__(800, 800, fullscreen = False)
+        self.x, self.y = 0, 0
+
+        self.player = pyglet.media.Player()
+        self.player.queue(pyglet.media.load("video.mp4"))
+        self.sprites = {'video' : None}
+
+        self.alive = 1
+
+    def on_draw(self):
+        self.render()
+
+    def on_close(self):
+        self.alive = 0
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        pass
+
+    def on_mouse_release(self, x, y, button, modifiers):
+        pass
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        pass
+
+    def on_mouse_drag(self, x, y, dx, dy, button, modifiers):
+        pass
+
+    def on_key_release(self, symbol, modifiers):
+        pass
+
+    def on_key_press(self, symbol, modifiers):
+        if symbol == 65307: # [ESC]
+            self.alive = 0
+        elif symbol == key.LCTRL:
+            self.player.play()
+
+    def render(self):
+        self.clear()
+
+        if self.player.playing:
+            if self.sprites['video'] is None:
+                texture = self.player.get_texture()
+                if texture:
+                    self.sprites['video'] = pyglet.sprite.Sprite(texture)
+            else:
+                self.sprites['video'].draw()
+
+        self.flip()
+
+    def run(self):
+        while self.alive == 1:
+            self.render()
+
+            # -----------> This is key <----------
+            # This is what replaces pyglet.app.run()
+            # but is required for the GUI to not freeze
+            #
+            event = self.dispatch_events()
+
+x = main()
+x.run()
+'''
